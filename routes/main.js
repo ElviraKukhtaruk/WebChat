47f87ddf0caee5bf4167da570de3a6de6d96a137
elvira <elvirakukhtaruk@gmail.com>
2021-07-28 23:05:28 +0200

    Added a new initial template page

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ let express = require("express");
 let router  = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('main');
+    res.sendFile('index.html');
 });
 
 module.exports = router;
